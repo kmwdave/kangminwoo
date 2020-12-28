6e8a72d87905bd1ee0560edb98f70c458d0ef3de
Kang <Kang@DESKTOP-7DLK7PP>
2020-12-28 16:39:37 +0900

    7교시 끝 커밋 1228

--- src/main/java/org/edu/service/BoardServiceImpl.java
+++ src/main/java/org/edu/service/BoardServiceImpl.java
@@ -8,6 +8,7 @@ import org.edu.dao.IF_BoardDAO;
 import org.edu.vo.BoardVO;
 import org.edu.vo.PageVO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service // 스프링빈으로 사용하기 위해서 애노테이션 명시
 public class BoardServiceImpl implements IF_BoardService{
@@ -27,7 +28,9 @@ public class BoardServiceImpl implements IF_BoardService{
 		return boardDAO.countBoard(pageVO);
 	}
 
-	@Override
+	// 아래 메서드는 구현이 2개 있는데 조회수 업데이트 정상처리 후 조회에러 발생시 업데이트한 조회수를 원상복귀시키는 작업. = 트랜잭션
+	@Transactional // 에러 발생시 update부분이 롤백.
+	@Override   
 	public BoardVO readBoard(Integer bno) throws Exception {
 		// bno번호에 해당하는 게시물 조회하는 쿼리 DAO연결 + 해당 게시물 조회수 업데이트 (아래)
 		boardDAO.updateViewCount(bno);

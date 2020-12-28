2c1e8485074eb0d435d43bfb84dd1657be1ff62e
Kang <Kang@DESKTOP-7DLK7PP>
2020-12-28 11:34:55 +0900

    1228 3교시 커밋

--- src/main/java/org/edu/service/IF_BoardService.java
+++ src/main/java/org/edu/service/IF_BoardService.java
@@ -7,4 +7,5 @@ import org.edu.vo.PageVO;
 
 public interface IF_BoardService {
 	public List<BoardVO> selectBoard(PageVO pageVO) throws Exception;
+	public int countBoard(PageVO pageVO) throws Exception;
 }
